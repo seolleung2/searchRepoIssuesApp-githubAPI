20fca13b516e27e5dbe91627df71b8a66082d80b
seolleung2 <seolleung22@gmail.com>
2023-02-12 12:24:12 +0900

    add: Pagination 적용 + IssueList, IssueItem 컴포넌트 작성 및 데이터 연결 - IssueList, IssueItem 컴포넌트 & UI CSS 작성 - Pagination 적용 - Github API 데이터 연결 적용

--- pages/[owner]/[repo].tsx
+++ pages/[owner]/[repo].tsx
@@ -1,24 +1,88 @@
-import React, { FunctionComponent } from 'react';
-import { BaseLayout } from '@components/layout';
-import { useIssues } from '@hooks/api/issues';
+import React, { FunctionComponent, useState } from 'react';
 import { useRouter } from 'next/router';
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import { Pagination } from '@mantine/core';
+import { BaseLayout } from '@components/layout';
+import { PAGE_SIZE } from '@Types/issues';
+import { getIssues } from '@apis/issues';
+import { ParsedUrlQuery } from 'querystring';
+import { IssueList } from '@components/issue';
 
-const RepositoryIssuesPage: FunctionComponent = () => {
+interface Props {
+  isError: boolean;
+  maxTotalIssues: number;
+}
+
+const RepositoryIssuesPage: FunctionComponent<Props> = ({
+  isError,
+  maxTotalIssues,
+}) => {
+  const [activePage, setPage] = useState(1);
+
   const router = useRouter();
-  const { route } = router;
-  const { data } = useIssues({
-    owner: 'codestates',
-    repo: 'naganda-client',
-    page: 1,
-    size: 5,
-  });
+  const { query } = router;
 
-  console.log(data);
+  if (isError) return <div>Error occur..</div>;
+
   return (
     <BaseLayout pageTitle="Issues">
-      <h2 className="py-3 text-2xl font-semibold">Related Issues</h2>
+      {'owner' in query && (
+        <IssueList
+          owner={query.owner as string}
+          repo={query.repo as string}
+          activePage={activePage}
+        />
+      )}
+      <div className="mt-2 flex w-full">
+        <Pagination
+          className="m-auto"
+          size="sm"
+          page={activePage}
+          onChange={setPage}
+          total={Math.ceil(maxTotalIssues / PAGE_SIZE)}
+          color="dark"
+        />
+      </div>
     </BaseLayout>
   );
 };
 
 export default RepositoryIssuesPage;
+
+export const getStaticProps: GetStaticProps = async ({
+  params,
+}: GetStaticPropsContext) => {
+  const { owner: ownerValue, repo: repoValue } = params as ParsedUrlQuery;
+
+  const owner = ownerValue as string;
+  const repo = repoValue as string;
+
+  let isError = false;
+  let maxTotalIssues = 0;
+
+  try {
+    const issues = await getIssues({
+      owner,
+      repo,
+      page: 1,
+      size: 100,
+    });
+
+    maxTotalIssues = issues.length;
+  } catch (error) {
+    isError = true;
+  }
+
+  return {
+    props: {
+      isError,
+      maxTotalIssues,
+    },
+  };
+};
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
